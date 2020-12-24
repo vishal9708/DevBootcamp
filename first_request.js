cec999bcf70bb7e61255a6d76217bad17526e453
vishal <vishalraj42000@gmail.com>
2020-12-24 18:26:10 +0530

    request

--- first_request.js
+++ first_request.js
@@ -1,1 +1,11 @@
-var request = request('');
+var request = require('request');
+request("http://www.googledsfef.com", function(error, response, body)
+{
+  if(!error)
+  {
+    console.log("Hey, It's working smoothly!!");
+  }
+  else{
+    console.log("something went wrong!!");
+  }
+})
