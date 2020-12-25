313453d2693b506fe6a1118f38fad93641886e9e
vishal <vishalraj42000@gmail.com>
2020-12-25 17:02:48 +0530

    mm

--- movie_search_app/app.js
+++ movie_search_app/app.js
@@ -4,6 +4,7 @@ rp(`https://www.omdbapi.com/?apikey=ccffb766&s=${val}`)
  .then((body)=> {
    var parsedData = JSON.parse(body);
    console.log(parsedData);
+   
  })
  .catch((err)=>{
    console.log(err);
