b8d117d2d33f4ef3c46948dddbcc4c4159946410
vishal <vishalraj42000@gmail.com>
2020-12-25 19:08:36 +0530

    .gitignore added

--- first_request.js
+++
@@ -1,9 +0,0 @@
-const requestPromise = require('request-promise');
-requestPromise("https://jsonplaceholder.typicode.com/users/1")
- .then((body) =>{
-    const parsedData = JSON.parse(body);
-    console.log(`${parsedData.name} lives in ${parsedData.address.city}`);
-  })
-  .catch((err) =>{
-    console.log('something went wrong!!', err);
-  });
--- movie_search_app/app.js
+++ movie_search_app/app.js
@@ -1,11 +1,11 @@
-var rp = require('request-promise')
-var val ='chori';
+const rp = require('request-promise');
+var val = 'chori';
 rp(`https://www.omdbapi.com/?apikey=ccffb766&s=${val}`)
  .then((body)=> {
-   var parsedData = JSON.parse(body);
+   const parsedData = JSON.parse(body);
    console.log(parsedData);
-   
  })
+
  .catch((err)=>{
    console.log(err);
  });
