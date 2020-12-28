033c77e09fecdeea8b60eba73e4916edb10ae23e
vishal <vishalraj42000@gmail.com>
2020-12-28 15:00:50 +0530

    fg

--- movie_search_app/app.js
+++ movie_search_app/app.js
@@ -1,22 +1,7 @@
-//window.addEventListener('load', getMovieDetails(), false);
+$(document).ready(function(){
+var url = "https://www.omdbapi.com/?apikey=ccffb766&s=star";
 
-function getMovieDetails(){
-
-var rp = require("request-promise");
-  //var url;
-  
-  //var org ="https://www.omdbapi.com/?";
-  //var api_key = "apikey=ccffb766&";
-  var name = window.document.getElementById('movie_title').value;
-
-  //url = org + api_key + name;
-  rp(`https://www.omdbapi.com/?apikey=ccffb766&s=${name}`)
-   .then((body)=>{
-         var parsed_data = JSON.parse(body);
-         console.log(parsed_data);
+   $.getJSON(url, function(data) {
+      console.log(data);
    })
-
-   .catch((error)=>{
-      console.log("error occurs!!!!",error);
-   })
-}
+});
