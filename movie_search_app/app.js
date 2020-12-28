9197bc3f0b2d99e852b5a1dbccec3b2c3207fc81
vishal <vishalraj42000@gmail.com>
2020-12-28 21:12:50 +0530

    covid

--- movie_search_app/app.js
+++ movie_search_app/app.js
@@ -1,7 +1,21 @@
-$(document).ready(function(){
-var url = "https://www.omdbapi.com/?apikey=ccffb766&s=star";
+//window.addEventListener('load', getMovieDetails(), false);
+function getMovieDetails(){
 
-   $.getJSON(url, function(data) {
-      console.log(data);
+var rp = require("request-promise");
+  //var url;
+  
+  //var org ="https://www.omdbapi.com/?";
+  //var api_key = "apikey=ccffb766&";
+  var name = document.getElementById('movie_title').value;
+
+  //url = org + api_key + name;
+  rp(`https://www.omdbapi.com/?apikey=ccffb766&s=${name}`)
+   .then((body)=>{
+         var parsed_data = JSON.parse(body);
+         console.log(parsed_data);
    })
-});
+
+   .catch((error)=>{
+      console.log("error occurs!!!!",error);
+   })
+}
