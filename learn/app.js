a9ab5296e45741139e7c276e2fcfbfc9f7a6a4e2
vishal <vishalraj42000@gmail.com>
2021-01-03 22:56:58 +0530

    dx

---
+++ learn/app.js
@@ -0,0 +1,12 @@
+var array=[];
+var value = prompt("What would you like to do?");
+
+while(value!="quit"){
+  if(value=="new"){
+    var task = prompt("Enter your to do?");
+    array.push(task);
+  }
+  else if(value=="list"){
+    console.log(array);
+  } 
+}
