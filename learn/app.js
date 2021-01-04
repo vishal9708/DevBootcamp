f7f75fdcf4fa80eb5e86d2948966f90c288f6cb6
vishal <vishalraj42000@gmail.com>
2021-01-04 09:13:12 +0530

    array

--- learn/app.js
+++ learn/app.js
@@ -1,14 +1,22 @@
-var array=[];
-var value = prompt("What would you like to do?");
+var todos = ["vishal"];
+var input = prompt("What would you like to do?");
 
-
-
-while(value!="quit"){
-  if(value=="new"){
-    var task = prompt("Enter your to do?");
-    array.push(task);
+  while(input!="quit"){
+    if(input=="new"){
+      var value = prompt("Enter to do?");
+      todos.push(value);
+    }
+    else if(input=="list")
+    {
+      todos.forEach(function(todo,i){
+        console.log(i+": "+todo);
+      })
+    }
+    else if(input=="delete")
+    {
+      console.log(`Todo Removed is: ${todos.pop()}`);
+    }
+    var input = prompt("What would you like to do?");
   }
-  else if(value=="list"){
-    console.log(array);
-  } 
-}
+  console.log("OK, you quit the app");
+
